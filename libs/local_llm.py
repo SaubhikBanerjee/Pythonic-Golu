33a8815265a89a171b295148a504785bc9fdc9e6
SaubhikBanerjee <saubbane@gmail.com>
2024-04-01 19:13:31 +0530

    major changes, part of Golu online 29-03-2024

--- libs/local_llm.py
+++ libs/local_llm.py
@@ -7,11 +7,15 @@ from libs.read_config import ReadConfig
 
 # Local CTransformers wrapper for Llama-2-7B-Chat
 my_config = ReadConfig("config/config.ini")
-local_llm = CTransformers(model=my_config.llm_path,
+try:
+    local_llm = CTransformers(model=my_config.llm_path,
                           model_type='llama',  # Model type Llama
                           config={'max_new_tokens': int(my_config.max_new_tokens),
                                   'temperature': float(my_config.temperature),
                                   'context_length': int(my_config.context_length)})
+except Exception as err:
+    local_llm = None
+    pass
 
 if __name__ == '__main__':
     print(type(local_llm))
